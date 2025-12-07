246b55d355b0078a2ab5eec5ce05215c0e7ee5e1
hereisphil <thereisphil@gmail.com>
2025-12-06 21:43:10 -0600

    Required refactoring due to attempted Vercel deployment. Created an all-in-one app/server at /server/server.js

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,24 @@
+import cors from "cors";
 import "dotenv/config";
+import express, { type Request, type Response } from "express";
+import morgan from "morgan";
 import connectDB from "./app/db/config.js";
-import app from "./app/index.js";
+import routeHandler from "./app/routes/index.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "API is running.",
+  });
+});
+
+app.use("/api/v1", routeHandler);
 
 connectDB();
 
