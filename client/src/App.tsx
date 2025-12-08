99f125bf98e2bbc9ddfe2267007971bbc5d1629a
hereisphil <thereisphil@gmail.com>
2025-12-08 12:25:22 -0600

    Slight refactoring of frontend, removed shadcn/ui - too complicatd to use for now. Will stick to standard TailwindCSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
-import { Route, Routes } from "react-router";
+import "./index.css";
+
+import { BrowserRouter, Route, Routes } from "react-router";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 
 export function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" index element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+export function Dashboard() {
+  return (
+    <main className="grow">
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
+
+export default Dashboard;
