4178072931a27d5ddc9fcef5b1ae55f96e7c266e
hereisphil <thereisphil@gmail.com>
2025-12-21 19:56:31 -0600

    Created loginUser.ts fetch - NOT using axios - I want to master Fetch API first.

---
+++ client/src/services/auth/loginUser.ts
@@ -0,0 +1,38 @@
+const baseUrl = "https://clientpulse-dusky.vercel.app/api/v1";
+
+// Specify the type of Promise Response I'm looking for:
+type LoginResponse = {
+  token: string;
+  email?: string;
+};
+
+const login = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}): Promise<LoginResponse | null> => {
+  try {
+    const response = await fetch(`${baseUrl}/auth/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    console.log("LOGIN RESPONSE >>>", response);
+    if (!response.ok) return null;
+    const data = await response.json();
+    console.log("DATA >>>", data);
+    if (data.token) {
+      localStorage.setItem("user", JSON.stringify(data));
+    }
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export default login;
