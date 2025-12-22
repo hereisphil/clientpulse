93e2760d7b3fcb7773077e4aec00c45dbe743664
hereisphil <thereisphil@gmail.com>
2025-12-21 22:01:43 -0600

    Readjusted app.ts to hopefully fix Vercel deployment:

--- server/app.ts
+++ server/app.ts
@@ -1,23 +1,23 @@
 import cors from "cors";
+import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
+app.get("/", (_req, res) =>
+  res.status(200).json({ success: true, message: "API is running." })
+);
 
-import "dotenv/config";
-import morgan from "morgan";
-import passport from "passport";
 import routeHandler from "./app/routes/index.js";
+app.use("/api/v1", routeHandler);
+
+import passport from "passport";
 import passportService from "./app/services/passport.js";
 
 app.use(passport.initialize());
 passportService();
-
-app.get("/", (_req, res) =>
-  res.status(200).json({ success: true, message: "API is running." })
-);
-app.use("/api/v1", routeHandler);
 
 export default app;
