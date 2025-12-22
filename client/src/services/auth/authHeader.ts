0d2e03a9854ddd6b62b4fc2a3343c1eab051e632
hereisphil <thereisphil@gmail.com>
2025-12-21 20:06:59 -0600

    Updated getAllClients to use authHeader

---
+++ client/src/services/auth/authHeader.ts
@@ -0,0 +1,16 @@
+const authHeader = () => {
+  try {
+    const storedUser = localStorage.getItem("user");
+    if (!storedUser) return undefined;
+    const user = JSON.parse(storedUser);
+    if (user && user.token) {
+      return { Authorization: user.token };
+    } else {
+      return undefined;
+    }
+  } catch {
+    return undefined;
+  }
+};
+
+export default authHeader;
--- client/src/services/getAllClients.ts
+++ client/src/services/getAllClients.ts
@@ -1,9 +1,13 @@
+import authHeader from "./auth/authHeader";
+
 const baseUrl = "https://clientpulse-dusky.vercel.app/api/v1";
 // const baseUrl = "http://127.0.0.1:8001/api/v1";
 
 const getAllClients = async () => {
   try {
-    const response = await fetch(`${baseUrl}/client`);
+    const response = await fetch(`${baseUrl}/client`, {
+      headers: authHeader(),
+    });
     console.log("getAllClients Raw Response >>>", response);
     if (response.ok) {
       const body = await response.json();
